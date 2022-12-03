34f2cb84b87fccbcd6c3d47d2ae03946b6beaaf1
Megan Davis <megcd93@gmail.com>
2022-12-03 09:16:26 -0600

    polished kind of

--- day3/rucksacks.hs
+++ day3/rucksacks.hs
@@ -5,15 +5,26 @@ import System.IO
 main = do
   handle <- openFile "input" ReadMode
   contents <- hGetContents handle
-  --  putStrLn $ "Part one is: " ++ show $ partOne contents
   putStrLn $ "Part one is: " ++ show (partOne contents)
   putStrLn $ "Part two is: " ++ show (partTwo contents)
 
+-- This should probably use a sum like part 2 but I gave up on fixing type errors to make that work
+partOne :: String -> Int
+partOne input_string =
+  let rucksacks = map splitMiddle $ lines input_string
+      value = getValue . findOverlap
+   in foldl (\acc x -> acc + value x) 0 rucksacks
+
+partTwo :: String -> Int
+partTwo input_string =
+  let badgeValues = map getValue . findBadges
+   in sum . badgeValues $ lines input_string
+
 -- Get the value of a character
-getValue :: Char -> Int
+getValue :: String -> Int
 getValue item =
   let itemPriorities = ['a' .. 'z'] ++ ['A' .. 'Z']
-   in fromMaybe 0 (item `elemIndex` itemPriorities) + 1
+   in fromMaybe 0 (head item `elemIndex` itemPriorities) + 1
 
 -- Split a string into two parts
 splitMiddle :: String -> (String, String)
@@ -25,19 +36,10 @@ splitMiddle rucksack =
 findOverlap :: (String, String) -> String
 findOverlap (rucksackA, rucksackB) = rucksackA `intersect` rucksackB
 
-partOne input_string =
-  let rucksacks = map splitMiddle $ lines input_string
-      value = getValue . head . findOverlap
-   in foldl (\acc x -> acc + value x) 0 rucksacks
-
--- Ha! Recursive functions :) find overlaps of a set of three strings
--- findOverlaps2 :: [String] -> [Char]
+-- Ha! Recursive functions :) find overlaps of each set of three strings
 findBadges :: [String] -> [String]
 findBadges (x : y : z : xs)
   | null xs = [findGroupBadge]
   | otherwise = findGroupBadge : findBadges xs
   where
     findGroupBadge = nub (findOverlap (x, y) `intersect` findOverlap (y, z))
-
--- partTwo :: String -> Int
-partTwo input_string = sum . map (getValue . head) $ findBadges $ lines input_string
