368b064a9818e31289875e7a06458aa0ed3e33ec
Megan Davis <megcd93@gmail.com>
2022-12-04 10:37:29 -0600

    overdoing DRY for fun

--- day4/camps.hs
+++ day4/camps.hs
@@ -5,41 +5,32 @@ import System.IO
 main = do
   handle <- openFile "input" ReadMode
   contents <- hGetContents handle
-  putStrLn $ "Part one is: " ++ show (partOne contents)
-  putStrLn $ "Part two is: " ++ show (partTwo contents)
+  putStrLn $ "Part one is: " ++ show (solvePart p1Folder contents)
+  putStrLn $ "Part two is: " ++ show (solvePart p2Folder contents)
 
--- Scan over each pair, add to accumulator if a is a subset of b
-partOne :: String -> Int
-partOne contents =
-  let assignments = importAssignments contents
-   in foldl myScanner 0 assignments
-  where
-    myScanner acc [a, b] =
-      if a `isInfixOf` b || b `isInfixOf` a
-        then acc + 1
-        else acc
+solvePart :: (Int -> [[Int]] -> Int) -> String -> Int
+solvePart folder = foldl folder 0 . importAssignments
 
--- Scan over each pair, add to accumulator if a and b have any overlap
-partTwo :: String -> Int
-partTwo contents =
-  let assignments = importAssignments contents
-   in foldl myScanner 0 assignments
-  where
-    myScanner acc [a, b] =
-      if not (null (a `intersect` b))
-        then acc + 1
-        else acc
+p2Folder :: Int -> [[Int]] -> Int
+p2Folder acc [a, b]
+  | not (null (a `intersect` b)) = acc + 1
+  | otherwise = acc
 
--- Makes an overly nested list of pairs of ranges
+p1Folder :: Int -> [[Int]] -> Int
+p1Folder acc [a, b]
+  | a `isInfixOf` b || b `isInfixOf` a = acc + 1
+  | otherwise = acc
+
+-- | Makes an overly nested list of pairs of ranges
 importAssignments :: String -> [[[Int]]]
 importAssignments contents =
   let assignPairs = map (splitOn ",") $ lines contents
    in map (map stringToRange) assignPairs
 
--- Converts range represented as "a-b" to [a..b] form
+-- | Converts range represented as "a-b" to [a..b] form
 stringToRange :: String -> [Int]
 stringToRange my_string =
   let splitStr = splitOn "-" my_string
-      rangeEnds = [read x :: Int | x <- splitStr]
+      rangeEnds = map (read :: String -> Int) splitStr
       makeRange [a, b] = [a .. b]
    in makeRange rangeEnds
